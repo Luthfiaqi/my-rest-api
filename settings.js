32f39ea6bf90c56f2097cd807025cef8cdcf5abb
Luthfiaqi <102463376+Luthfiaqi@users.noreply.github.com>
2023-11-26 07:47:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'Luthfi'// yourname
+global.MONGO_DB_URI = "mongodb+srv://luthfiaqil0:<password>@cluster0.pdlhtma.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "Luthfi9262@" //isi apa aja bebas
+global.your_email = "luthfiaqil0@gmail.com" //email
+global.email_password = "terg rriw xnyl zfik" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
